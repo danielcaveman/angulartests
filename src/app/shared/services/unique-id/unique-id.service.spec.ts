ad81caf2a99f6f780b84b69c2a0f5e42263bf724
danielcaveman <daniel_caverna@yahoo.com.br>
2022-03-02 11:43:16 -0300

    added new test

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,19 @@
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
-  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
+  })
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicated IDs when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
   })
 })
