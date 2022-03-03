70c5f3f1593b33966b038ea2a73960beb0278f29
danielcaveman <daniel_caverna@yahoo.com.br>
2022-03-03 09:59:14 -0300

    added spy

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -4,31 +4,38 @@ import { LikeWidgetComponent } from './like-widget.component';
 
 describe('LikeWidgetComponent', () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
       imports: [LikeWidgetModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(LikeWidgetComponent)
+    fixture = TestBed.createComponent(LikeWidgetComponent);
+
+    component = fixture.componentInstance;
   });
 
   it('should create component', () => {
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 
   it('should generate ID when input property is missing', () => {
-    const component = fixture.componentInstance;
     fixture.detectChanges();
     expect(component.id).toBeTruthy(); // not null as it was defined
   });
 
   it('should receive an input property ID', () => {
-    const component = fixture.componentInstance;
     const someId = 'someId'
     component.id = someId;
     fixture.detectChanges();
-    expect(component.id).toBe(someId); // not null as it was defined
+    expect(component.id).toBe(someId);
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    should trigger emission when called`, () => {
+    spyOn(component.liked, 'emit')
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 });
